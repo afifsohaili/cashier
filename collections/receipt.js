7029a4279bf8061b5198caf998597203dd3506a4
afifsohaili <afifnajib@gmail.com>
2015-10-10 17:42:33 +0800

    Add dummy data. Add schemas namespace

--- collections/receipt.js
+++ collections/receipt.js
@@ -1,3 +1,20 @@
-Collections.receipts = new Mongo.Collection("receipts");
+Collections.Receipts = new Mongo.Collection("receipts");
 
-
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Collections.Receipts.find().count() === 0) {
+      Collections.Receipts.insert({
+        items: [
+          {
+            name: "Bawal Crepe - Bidang 60",
+            price: 30.99,
+          },
+          {
+            name: "Batik Shawl",
+            price: 20.50,
+          }
+        ]
+      });
+    }
+  });
+}
