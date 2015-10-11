9b5643666f791fbacaed864b7104b1bcee29c045
afifsohaili <afifnajib@gmail.com>
2015-10-11 14:28:27 +0800

    Add mobile settings

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+App.info({
+  name: 'Keshier',
+  description: "An Android app built with Meteor",
+  version: "0.0.1"
+});
+
+App.icons({
+  "android_mdpi": "resources/icons/mipmap-mdpi/ic_launcher.png",
+  "android_hdpi": "resources/icons/mipmap-hdpi/ic_launcher.png",
+  "android_xhdpi": "resources/icons/mipmap-xhdpi/ic_launcher.png"
+});
